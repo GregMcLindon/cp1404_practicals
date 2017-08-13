1879f73c3ae16dab97b5d1f3df63adde7492b0b0
GregMcLindon <gregory.mclindon@my.jcu.edu.au>
2017-08-13 22:53:55 +1000

    broken_score refactored

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -1,28 +1,20 @@
-"""
-CP1404/CP5632 - Practical
-Broken program to determine score status
-"""
+def main():
+    """
+    CP1404/CP5632 - Practical
+    Broken program to determine score status
+    """
+
+def get_result(score):
+    if not score >= 0 or not score <= 100:
+        result = "Invalid score"
+    elif score > 90:
+        result = "Excellent"
+    elif score > 50:
+        result = "Passable"
+    else:
+        result = "Bad"
+    return result
+
 score = float(input("Enter score: "))
-""""
-# broken code
-if score < 0:
-    print("Invalid score")
-else:
-    if score > 100:
-        print("Invalid score")
-    if score > 50:
-        print("Passable")
-    if score > 90:
-    print("Excellent")
-if score < 50:
-    print("Bad")
-"""
-# fixed code by G.McLindon 29/7/2017
-if not score >= 0 or not score <= 100:
-    print("Invalid score")
-elif score > 90:
-    print("Excellent")
-elif score > 50:
-    print("Passable")
-else:
-    print("Bad")
+print(get_result(score))
+main()
