c9f1ee90190ff5a2c5e471cc9ee057615751bc46
GregMcLindon <gregory.mclindon@my.jcu.edu.au>
2017-08-19 10:20:53 +1000

    name lower case removed from output

--- prac_04/warmup_exercise.py
+++ prac_04/warmup_exercise.py
@@ -1,8 +1,8 @@
 VOWELS = "aeiou"
-name = input('What is your name?').lower()
+name = input('What is your name?')
 count_letters = len(name)
 vowel_count = 0
 for char in name:
-    if char in VOWELS:
+    if char.lower() in VOWELS:
        vowel_count = vowel_count + 1
 print(str.format('Out of {} letters, {} has {} vowels', count_letters, name, vowel_count))
