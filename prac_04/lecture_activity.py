481ff9be3a42994e2cc2066dab3de9b3ae1e1955
GregMcLindon <gregory.mclindon@my.jcu.edu.au>
2017-08-19 11:51:39 +1000

    adds integer and non-blank score validation to lecture activity

--- prac_04/lecture_activity.py
+++ prac_04/lecture_activity.py
@@ -1,6 +1,15 @@
 scores = list()
-score = int(input("Score: "))
-while score >= 0:
-    scores.append(score)
-    score = int(input("Score: "))
-print("Your highest score is ", max(scores))
+valid_score = False
+while not valid_score:
+    try:
+        score = int(input("Score: "))
+        while score >= 0:
+            scores.append(score)
+            score = int(input("Score: "))
+        valid_score = True
+    except:
+        print("Score must be an integer")
+if scores == []:
+    print("No valid scores entered")
+else:
+    print("Your highest score is ", max(scores))
