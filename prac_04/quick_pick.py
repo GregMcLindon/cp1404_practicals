4e58ab91353057823e6002896ad10ba03014f4b6
GregMcLindon <gregory.mclindon@my.jcu.edu.au>
2017-08-20 14:10:32 +1000

    adds print formatted to quick_pick application

--- prac_04/quick_pick.py
+++ prac_04/quick_pick.py
@@ -7,27 +7,28 @@ def main():
     #ask user how many games they would like to play
     valid_pick = False
     while not valid_pick:
-        picks_selected = input("How many quick picks?")
+        picks_selected = input("How many quick picks? ")
         try:
             picks_selected = int(picks_selected)
             if picks_selected > 0:
                 valid_pick = True
                 numbers_selected = generate_numbers(picks_selected)
-                print(numbers_selected)
+                for game_numbers in numbers_selected:
+                    print(*game_numbers)
             else:
                 print("Select valid number greater than 0")
         except:
             print("Select valid number greater than 0")
 
 def generate_numbers(games_playing: int)->list:
-    #generate random numbers and return as a list
+    #returns draw list of games each with distinct set of random numbers
     game_numbers=[]
     NUMBERS_PER_GAME = 6
     draw_numbers=[]
     while len(draw_numbers) < games_playing:
         while len(game_numbers) < NUMBERS_PER_GAME:
-            random_number = random.randint(1,45)
-            if not random_number in draw_numbers:
+            random_number = str("{:2}".format(random.randint(1,45)))
+            if not random_number in game_numbers:
                 game_numbers.append(random_number)
         game_numbers.sort()
         draw_numbers.append(game_numbers)
