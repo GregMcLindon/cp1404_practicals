17eee819ad4eb19a43db76768b0ae9b36fd19e18
GregMcLindon <gregory.mclindon@my.jcu.edu.au>
2017-09-30 11:30:44 +1000

    add exception handling to color_names.py

--- prac_05/color_names.py
+++ prac_05/color_names.py
@@ -1,11 +1,16 @@
 """CP1404 Practical week 5: look up color codes"""
 
-color_names = {"AliceBlue": "#f0f8ff", "AntiqueWhite1": "#ffefdb", "azure4": "#838b8b", "beige": "#f5f5dc",
+color_dict = {"AliceBlue": "#f0f8ff", "AntiqueWhite1": "#ffefdb", "azure4": "#838b8b", "beige": "#f5f5dc",
                "bisque1": "#ffe4c4", "bisque2": "#eed5b7", "bisque3": "#cdb79e", "bisque4": "#8b7d6b",
                "black": "#000000", "BlanchedAlmond": "#ffebcd", "cyan1": "#00ffff", "cyan2": "#00eeee",
                "cyan3": "#00cdcd", "cyan4": "#008b8b", "DarkGoldenrod": "#b8860b", "DarkGoldenrod1": "#ffb90f",
                "DarkGoldenrod2": "#eead0e"}
-color_name = input("Enter color name: ")
-while color_name!="":
-    print(color_names[color_name])
-    color_name = input("Enter color name: ")
+color_search = input("Enter color name: ")
+while color_search != "":
+    try:
+        print(color_dict[color_search])
+    except:
+        print("Enter valid name from following list:")
+        for color in sorted(color_dict):
+            print("{:5}{}".format("",color))
+    color_search = input("Enter color name: ")
