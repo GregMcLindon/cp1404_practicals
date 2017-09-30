233015b01d8284d2ff9d4ed86bf8c24312b824c6
GregMcLindon <gregory.mclindon@my.jcu.edu.au>
2017-10-01 09:45:10 +1000

    add current year to guitar class parameters

--- prac_07/guitar.py
+++ prac_07/guitar.py
@@ -1,11 +1,12 @@
 class Guitar:
     """CP1403 guitar types class"""
 
-    def __init__(self, name: str="", year: int=0, cost: float=0):
+    def __init__(self, name: str="", year: int=0, cost: float=0, current_year: int=0):
         """initialise a guitar type"""
         self.name = name
         self.year = year
         self.cost = cost
+        self.current_year = current_year
 
     def __str__(self):
         """Used for printing guitar details"""
@@ -14,7 +15,7 @@ class Guitar:
 
     def get_age(self)->int:
         """calculate age of guitar relative to the year this code was written (2017)"""
-        return 2017 - self.year
+        return self.current_year - self.year
 
     def is_vintage(self)->bool:
         return self.get_age() > 50
