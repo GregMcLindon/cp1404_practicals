dbae47ad785ea2873b08b566b64eedf87c62a8c4
GregMcLindon <gregory.mclindon@my.jcu.edu.au>
2017-10-01 17:11:21 +1000

    Add date class and date add function

--- prac_07/date.py
+++ prac_07/date.py
@@ -16,14 +16,54 @@ class Date:
 
     def add_days(self, days):
         """Calculate new date by adding days"""
-        while days > 0:
+        while abs(days) > 0:
+            is_leap_year = check_leap_year(self.year)
+            days_in_month = get_days_in_month(self.month, is_leap_year)
+
+            if self.day == 0:  # 0 indicates day was reset due to negative date add
+                self.day = days_in_month
+
+            if days >= 1:  # add days
+                if (self.day + days) > days_in_month:
+                    self.month += 1
+                    days -= (days_in_month - self.day) + 1
+                    self.day = 1
+                    if self.month == 13:
+                        self.year += 1
+                        self.month = 1
+                else:
+                    self.day += days
+                    days = 0
+            else:  # subtract days
+                if (self.day + days) <= 0:
+                    self.month += -1
+                    days += self.day
+                    if self.month == 0:
+                        self.year += -1
+                        self.month = 12
+                        is_leap_year = check_leap_year(self.year)
+                    self.day = get_days_in_month(self.month, is_leap_year)  # reset day number to days in previous month
+                else:
+                    self.day += days
+                    days = 0
+
+        return "{}/{}/{}".format(self.day, self.month, self.year)
 
 
+def check_leap_year(year_to_check)->bool:
+    """checks to see if the given year is a leap year"""
+    return year_to_check % 4 == 0 and (year_to_check % 100 != 0 or year_to_check % 400 == 0)
 
-        if distance > self.fuel:
-            distance = self.fuel
-            self.fuel = 0
+
+def get_days_in_month(month_to_get, is_leap_year):
+    """return the number of days in a given month"""
+    if month_to_get in [4, 6, 9, 11]:
+        days_in_month = 30
+    elif month_to_get == 2:
+        if is_leap_year:
+            days_in_month = 29
         else:
-            self.fuel -= distance
-        self.odometer += distance
-        return distance
+            days_in_month = 28
+    else:
+        days_in_month = 31
+    return days_in_month
---
+++ prac_07/date_test.py
@@ -0,0 +1,30 @@
+"""CP1403 prac_07 code written by Greg McLindon"""
+from prac_07.date import Date
+
+#  Current date test data
+# CURRENT_DAY = 1
+# CURRENT_MONTH = 10
+# CURRENT_YEAR = 2017
+
+#  Leap Year test data
+CURRENT_DAY = 29
+CURRENT_MONTH = 2
+CURRENT_YEAR = 2017
+
+
+def main():
+    """test the date class is working"""
+    my_date = Date(CURRENT_DAY, CURRENT_MONTH, CURRENT_YEAR)
+    days_to_add = None
+    while days_to_add != 0:
+        days_to_add = input("Enter number of days to add: ")
+        try:
+            days_to_add = int(days_to_add)
+            if days_to_add != 0:
+                print("Current date: {}".format(my_date))
+                my_date.add_days(days_to_add)
+                print("New date: {}".format(my_date))
+        except:
+            print("invalid date")
+
+main()
