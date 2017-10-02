6bfaee397bd2cff85dce06ee738818beba38922f
GregMcLindon <gregory.mclindon@my.jcu.edu.au>
2017-10-02 17:30:00 +1000

    Add while loop structure to Taxi Simulator application

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -4,14 +4,35 @@ Written by Greg McLindon"""
 
 from prac_08.taxi import Taxi
 from prac_08.silver_service_taxi import SilverServiceTaxi
-
+MENU = "q)uit, c)hoose taxi, d)rive"
+QUIT = 'q'
+CHOOSE = 'c'
+DRIVE = 'd'
 
 def main():
     """"run the simulator"""
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2),
              SilverServiceTaxi("Hummer", 200, 4)]
-    for taxi in taxis:
-        print(str(taxi))
+    print("Let's drive!")
+    total_fare = 0
+    menu_choice = input(">>> ").lower()
+    while menu_choice.lower() != QUIT:
+        print(MENU)
+        if menu_choice == CHOOSE:
+            'TODO - create function for choose'
+            pass
+        elif menu_choice == DRIVE:
+            'TODO - call drive method'
+            'TODO - update fare cost based on drive'
+            pass
+        else:
+            print("Invalid selection")
+        menu_choice = input(">>> ").lower()
+
+    print("Total trip cost: ${:.2f}\nTaxis are now:".format(total_fare))
+    for i, taxi in enumerate(taxis):
+        print("{} - {}".format(i, str(taxi)))
+
 #     my_taxi = Taxi("Prius 1", 100)
 #     my_taxi.drive(40)
 #     print_fare_details(my_taxi)
