7bfbcebda8ab475ed0272f96945a2d8a7a72a167
GregMcLindon <gregory.mclindon@my.jcu.edu.au>
2017-10-07 13:35:18 +1000

    fix quick_pick number stored as string and languages year default value string following peer feedback

--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -1,7 +1,7 @@
 class ProgrammingLanguage:
     """CP1403 programming language class."""
 
-    def __init__(self, name: str="", typing: str="", reflection: bool=False, year: int="0000"):
+    def __init__(self, name: str="", typing: str="", reflection: bool=False, year: int=0):
         """initialise a language type"""
         self.name = name
         self.typing = typing
